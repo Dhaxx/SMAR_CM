c5eaa231f65715ecc1bd0ba692fa2d4d1d8b4b3e
psercotrim <pinguixs@gmail.com>
2024-02-26 14:22:31 -0300

    Commit Teste Paulo

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from modulos.compras import base
 from modulos.compras import solicitacoes
 
 def main():
-    # base.cadunimedida()
-    # base.grupo_e_subgrupo()
-    #base.cadest()
-    # base.almoxarifado()
-    # base.centro_custo()
+    base.cadunimedida()
+    base.grupo_e_subgrupo()
+    base.cadest()
+    base.almoxarifado()
+    base.centro_custo()
     solicitacoes.solicitacoes()
 
 if __name__ == '__main__':
